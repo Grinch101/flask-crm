14706b900cd0f03db9e85d8201312c66c3458d14
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-05 15:22:46 +0330

    using flash

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from contact import Contact
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
 
 ## View Function ##############
+
 phonbook = Contact()
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def index():
+
+    if request.form:
+        
+        input_name = request.form['Name']
+        input_number = request.form['Number']
+        username = request.form['client_name']
+        phonbook.insert(username,input_name,input_number)
+
+        flash(f'{input_number} for {input_name} has been saved')
+
+        return redirect(url_for('index'))
 
     return render_template('index.html')
 
 
-@app.route('/saved', methods=["POST"])
-def saved():
 
-    username = request.form['client_name']
-    input_name = request.form['Name']
-    input_number = request.form['Number']
-
-    
-    phonbook.insert(username,input_name , input_number)
-
-
-    return render_template('index.html' , mylist=phonbook.GetAll(), username = username  )
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
