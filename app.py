99382091af5132d03f4d9fcf2d62db9c9dd11996
Mr Grinch <masoud.kameli@gmail.com>
2021-01-05 11:11:27 +0330

    Update using .bat file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
-from contact import Contact, db
+from contact import Contact
 
-app = Flask(__name__, template_folder='A')
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
 
 ## View Function ##############
@@ -19,25 +19,27 @@ def saved():
     input_name = request.form['Name']
     input_number = request.form['Number']
 
-    db.append( Contact(username , input_name , input_number) )
+    # db.append( Contact(username , input_name , input_number) )
+    phonbook = Contact()
+    phonbook.insert(username,input_name , input_number)
     
-    contact_list = [i.entries['name'] for i in db]
-    number_list = [i.entries['phone'] for i in db]
-    owners_list = [i.entries['client_name'] for i in db]
-    contactlist = list(zip(owners_list , number_list , contact_list))
+    # contact_list = [i.entries['name'] for i in db]
+    # number_list = [i.entries['phone'] for i in db]
+    # owners_list = [i.entries['client_name'] for i in db]
+    # contactlist = list(zip(owners_list , number_list , contact_list))
 
-    def entry_count(owners_list = owners_list):
-        # This gonna be NASTY, Sorry!
-        Z = owners_list # only to use shorter name!
-        owner_freq = list(zip([Z.count(i) for i in set(Z)],set(Z)))
+    # def entry_count(owners_list = owners_list):
+    #     # This gonna be NASTY, Sorry!
+    #     Z = owners_list # only to use shorter name!
+    #     owner_freq = list(zip([Z.count(i) for i in set(Z)],set(Z)))
         
-        reporter = []
-        for i in owner_freq:
-            reporter.append(f'{i[1]} has saved {i[0]} contact(s)')
-        return reporter
+    #     reporter = []
+    #     for i in owner_freq:
+    #         reporter.append(f'{i[1]} has saved {i[0]} contact(s)')
+    #     return reporter
         
 
-    return render_template('index.html' , mylist=contactlist, username = username  , db = entry_count() )
+    return render_template('index.html' , mylist=phonbook.GetAll(), username = username  )
 
 if __name__ == '__main__':
     app.run(debug = True)
--- contact.py
+++ contact.py
@@ -1,18 +1,22 @@
 
 class Contact():
-    def __init__(self,client_name, name, phone):
+    def __init__(self):
+        
+        self.db = []
+
+    def insert(self,client_name,name,phone):
         self.client_name = client_name
         self.name = name
         self.phone = phone
-
-        entries = {'client_name':client_name,
+        entry = {   'client_name':self.client_name,
                     'name':name,
-                    'phone': phone
-                  }
-        self.entries = entries
-      
-      def __repr__(self):
-
+                    'phone': phone  }
+        self.db.append(entry)
+    
+    def GetAll(self):
+      return self.db
         
-
-db = []
+      
+      def __len__(self):
+        return len(self.db)
+    
