e7fb8e8158b7f4cdc5c7db4b99063e62e2da6142
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-05 15:36:45 +0330

    flash messages + saved() route

--- app.py
+++ app.py
@@ -10,18 +10,21 @@ phonbook = Contact()
 @app.route('/', methods=['GET','POST'])
 def index():
 
-    if request.form:
-        
-        input_name = request.form['Name']
-        input_number = request.form['Number']
-        username = request.form['client_name']
-        phonbook.insert(username,input_name,input_number)
-
-        flash(f'{input_number} for {input_name} has been saved')
-
-        return redirect(url_for('index'))
-
     return render_template('index.html')
+
+
+@app.route('/saved' , methods=['GET' , "POST"])
+def saved():
+    
+    input_name = request.form['Name']
+    input_number = request.form['Number']
+    username = request.form['client_name']
+    phonbook.insert(username,input_name,input_number)
+
+    flash(f'{input_number} for {input_name} has been saved')
+
+    return redirect(url_for('index'))
+
 
 
 
