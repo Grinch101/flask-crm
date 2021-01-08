f7b6eebbc71748b6138ea324c1f939367b22e8e9
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-08 18:13:53 +0330

    helper dictionaries added

--- contact.py
+++ contact.py
@@ -1,9 +1,14 @@
 
+
 class Contact():
     def __init__(self):
         
         self.db = []
         self.id = 0
+        # helper dictionaries:
+        
+        self.id_index_dict = {}
+        self.id_phone_dict = {}
     def insert(self,client_name,name,phone):
         
         entry = {   'client_name':client_name,
@@ -11,16 +16,18 @@ class Contact():
                     'phone': phone,
                     'id':  self.id }
         self.db.append(entry)
-        self.id += 1
+        
+        
+        ## side dics:
+        self.id_phone_dict[self.id] = phone
+        self.id_index_dict[self.id] = len(self.db) - 1
 
+        self.id += 1
 
     def find_index(self,id):
         
         
-        for i , dic in enumerate(self.db):
-            if dic['id'] == id:
-                index = i
-        return index
+        return self.id_index_dict[id]
 
     def find_val(self,id): # returns value corresponding to i
         
@@ -34,8 +41,22 @@ class Contact():
 
 
     def delete(self, id):
-        delval = self.find_val(id)
-        self.db.remove(delval)
+        
+        
+        try:
+            self.db.remove(self.find_val(id))
+
+            del self.id_index_dict[id]
+            del self.id_phone_dict[id]          
+    
+            for id in range(id+1,len(self.db)):
+            
+                self.id_index_dict[id] -= 1
+        
+        except:
+            pass
+
+        
     
 
     def update(self , id , new_client , new_name , new_phone):
