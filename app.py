fa2d3a26bbf609e1f60e4d76ebf1ef68ffb7a6ba
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-10 15:29:20 +0330

    logged_in function created

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, make_response
 from contact import Contact
+from user import User
 import names
 import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
 
-## View Function ##############
+
 
 #  injecting some functions to Jinja2 
 @app.context_processor
@@ -19,18 +20,45 @@ def inject_func():
 
 
 phonebook = Contact()
+
+## View Function ##############
+
 @app.route('/', methods=['POST', "GET"])
 def index():
+    if request.cookies.get('email'):
 
-    return render_template('index.html')
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
 
+
+
+@app.route('/logged_in', methods=['GET',"POST"])
+def logged_in():
+
+    users_handler = User()
+   
+    email = request.form.get("inputEmail")
+    password = request.form.get("inputPassword")
+
+    if users_handler.validate(email , password):
+
+        return redirect(url_for('index'))
+
+    else:
+        users_handler.register(email,password)
+
+        response = make_response(render_template('index.html'))
+        response.set_cookie('email',email)
+
+        return response
 
 @app.route('/saved' , methods=["POST"])
 def saved():
     
     input_name = request.form['Name']
     input_number = request.form['Number']
-    username = request.form['client_name']
+    username = request.cookies.get('email')
     phonebook.insert(username,input_name,input_number)
 
     flash(f'{input_number} for {input_name} has been saved')
@@ -41,7 +69,11 @@ def saved():
 @app.route('/table' , methods=["GET","POST"])
 def table():
     
-    return render_template('list.html' , mylist = phonebook.db[:10] )
+    username = request.cookies.get('email')
+
+    contact_list = [ i for i in phonebook.db if i.get('client_name')==username ]
+
+    return render_template('list.html' , mylist = contact_list )
 
 
 @app.route('/delete' , methods=["GET","POST"])
@@ -56,18 +88,18 @@ def delete():
 
 
 
-i = 0
-while i <= 150:
+# i = 0
+# while i <= 150:
 
-    user_length = random.randint(1,5)
-    for j in range(user_length):
-        client_name = names.get_first_name()
-        book_length = random.randint(1,10)
-        for k in range(book_length):
-            name = names.get_full_name()
-            phone = int(random.random()*10000000000)
-            phonebook.insert(client_name,name,phone)
-            i += 1
+#     user_length = random.randint(1,5)
+#     for j in range(user_length):
+#         client_name = names.get_first_name()
+#         book_length = random.randint(1,10)
+#         for k in range(book_length):
+#             name = names.get_full_name()
+#             phone = int(random.random()*10000000000)
+#             phonebook.insert(client_name,name,phone)
+#             i += 1
 
 
 if __name__ == "__main__":
