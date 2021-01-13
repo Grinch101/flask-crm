87b94ddabfb8e41b0792f0044b0a7abcd7311afc
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-14 00:43:23 +0330

    removing unncessary dictionaries

--- contact.py
+++ contact.py
@@ -6,9 +6,9 @@ class Contact():
         self.db = []
         self.id = 0
         # helper dictionaries:
-        self.id_userid = {}
+        # self.id_userid = {}
         self.id_index_dict = {}
-        self.id_phone_dict = {}
+        # self.id_phone_dict = {}
         self.userid_id = {}
         
     def insert(self,userid,client_name,name,phone):
@@ -27,9 +27,9 @@ class Contact():
         else:
             self.userid_id[userid] = [self.id]
 
-        self.id_phone_dict[phone] = self.id
+        # self.id_phone_dict[phone] = self.id
         self.id_index_dict[self.id] = len(self.db) - 1
-        self.id_userid[self.id] = userid
+        # self.id_userid[self.id] = userid
         self.id += 1
 
     def find_index(self,id):
@@ -54,7 +54,7 @@ class Contact():
             self.db.remove(self.find_val(id))
 
             del self.id_index_dict[id]
-            del self.id_phone_dict[id]          
+            # del self.id_phone_dict[id]          
     
             for id in range(id+1,len(self.db)):
             
@@ -65,27 +65,25 @@ class Contact():
 
         
     
-
-    def update(self , id , new_client , new_name , new_phone):
+    def update(self , id, new_value , entity = 'client_name' ):
         
-        self.delete(id)
-        userid = self.id_userid[id]
-        
-        new_entry = {'userid':userid,
-                    'client_name':new_client,
-                    'name':new_name,
-                    'phone': new_phone,
-                    'id':id  }
+        # self.delete(id)
+        # userid = self.id_userid[id]
+        old_entry = self.find_val(id)
+        new_entry = old_entry
 
-        self.db.append(new_entry)
+        new_entry[entity] = new_value
+        index = self.find_index(id)
+        self.db[index] = new_entry
 
-        self.db = sorted( self.db , key = lambda dic: dic['id'] )
+        # self.db = sorted( self.db , key = lambda dic: dic['id'] )
+        # edit side dics:
 
     
     def find_book(self, userid):
         try:
-            user_list_of_id = self.userid_id.get(userid)
-            return [self.find_val(id) for id in user_list_of_id]
+            entry_list = self.userid_id.get(userid) # list of entries, belong to userid
+            return [self.find_val(id) for id in entry_list]
         except:
             return []
 
