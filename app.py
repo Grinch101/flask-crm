45890f23ca817d95c1d8d89540bd757620ba33b9
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-17 23:30:53 +0330

    insert and register edited

--- app.py
+++ app.py
@@ -9,23 +9,27 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
 
 #########################
+
+
 def login_required(func):
     @wraps(func)
     def wrap(*args, **kwargs):
         if request.cookies.get('user_id'):
             return func(*args, **kwargs)
         else:
-            
+
             return redirect(url_for(('login_form')))
     return wrap
 
-#  injecting some functions to Jinja2 
+#  injecting some functions to Jinja
+
+
 @app.context_processor
 def inject_func():
     return dict(enumerate=enumerate,
-                list = list,
-                range = range,
-                len = len)
+                list=list,
+                range=range,
+                len=len)
 
 
 ## initiate ###################
@@ -34,17 +38,17 @@ users_handler = User()
 
 ## View Function ##############
 
+
 @app.route('/', methods=["GET"])
 @login_required
 def index():
     userid = int(request.cookies.get('user_id'))
     entry = users_handler.find_val(userid)
     username = entry['client_name']
-    return render_template('index.html' , username = username)
- 
+    return render_template('index.html', username=username)
 
 
-@app.route('/login' , methods=["GET"])
+@app.route('/login', methods=["GET"])
 def login_form():
     flash("Please Login first!")
     return render_template('login.html')
@@ -56,26 +60,26 @@ def login_check():
     email = request.form.get("inputEmail")
     password = request.form.get("inputPassword")
 
-    if users_handler.validate(email , password):
+    if users_handler.validate(email, password):
 
         userid = users_handler.find_userid_by_email(email)
         response = make_response(redirect(url_for('index')))
-        response.set_cookie('user_id' , str(userid))    
+        response.set_cookie('user_id', str(userid))
         return response
 
     else:
-    
+
         flash('Wrong Email or Password. Please try again, or Sign-up!')
         return redirect(url_for('login_form'))
-    
 
-@app.route('/signup', methods=["GET"] )
+
+@app.route('/signup', methods=["GET"])
 def signup_form():
 
     return render_template('signup.html')
 
 
-@app.route('/signup', methods=["POST"] )
+@app.route('/signup', methods=["POST"])
 def signup():
 
     email = request.form.get("inputEmail")
@@ -84,64 +88,62 @@ def signup():
 
     if not users_handler.old_user(email):
 
-        users_handler.register(client_name,email,password)
+        users_handler.register(client_name, email, password)
         userid = users_handler.email_userid[email]
 
         response = make_response(redirect(url_for('index')))
-        response.set_cookie('user_id' , str(userid)) 
+        response.set_cookie('user_id', str(userid))
         return response
     else:
         flash('Email in use, please login')
         return redirect(url_for('signup_form'))
 
 
-
-@app.route('/saved' , methods=["POST"])
+@app.route('/saved', methods=["POST"])
 @login_required
 def saved():
-    
+
     input_name = request.form['Name']
     input_number = request.form['Number']
     userid = request.cookies.get('user_id')
     userid = int(userid)
     entry = users_handler.find_val(userid)
-    username = entry.get('client_name')
 
-    phonebook.insert(userid,username,input_name,input_number)
+    phonebook.insert(entry)
 
     flash(f'{input_number} for {input_name} has been saved')
 
     return redirect(url_for('index'))
 
 
-@app.route('/table' , methods=["GET"])
+@app.route('/table', methods=["GET"])
 @login_required
 def table():
-    
+
     userid = request.cookies.get('user_id')
     userid = int(userid)
     contact_list = phonebook.find_book(userid)
 
-    return render_template('list.html' , mylist = contact_list )
+    return render_template('list.html', mylist=contact_list)
 
 
-
-@app.route('/delete' , methods=["POST"])
+@app.route('/delete', methods=["POST"])
 @login_required
-def delete(): 
+def delete():
 
     id = int(request.form.get("DELETE"))
     phonebook.delete(id)
     flash(f"ID:{id} Deleted")
 
     return redirect(url_for('table'))
-  
-@app.route('/logout' , methods=['POST'])
+
+
+@app.route('/logout', methods=['POST'])
 @login_required
 def logout():
-    
+
     response = make_response(redirect(url_for('login_form')))
-    response.set_cookie('user_id' , "" , max_age = 0)
+    response.set_cookie('user_id', "", max_age=0)
     return response
 
 
@@ -163,13 +165,13 @@ def behind():
         dic2 = users_handler.email_userid
 
         return render_template('behind-the-scene.html', dic1=dic1,
-                                                        dic2=dic2,
-                                                        list1=list1,
-                                                        list2 = list2,
-                                                        list3=list3)
-    
-    else :
-        return render_template('behind-the-scene.html' )
+                               dic2=dic2,
+                               list1=list1,
+                               list2=list2,
+                               list3=list3)
+
+    else:
+        return render_template('behind-the-scene.html')
 
 
 # i = 0
