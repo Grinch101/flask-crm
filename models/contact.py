f407f8743583674cd41e9f0dd30a89ea54ef4f01
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-19 10:33:54 +0330

    delete removed, using super() instead

--- models/contact.py
+++ models/contact.py
@@ -7,30 +7,17 @@ class Contact(BaseModel):
         self.userid_id = {}
         super().__init__()
 
-
     def add(self, entry):
-        
-        id  = super().add(entry)
+
+        id = super().add(entry)
 
         # extract the userid back, add to helper dictionaries:
         userid = entry['userid']
-        
+
         if self.userid_id.get(userid):
             self.userid_id[userid].append(id)
         else:
             self.userid_id[userid] = [id]
-
-
-    def delete(self, id):
-
-        try:
-            super().delete(id)
-            del self.id_index[id]
-            for id in range(id+1, len(self.list)+1):
-                self.id_index[id] -= 1
-        except:
-            pass
-
 
     def find_book(self, userid):
         try:
