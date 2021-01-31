a32e1374a671cd7c433099770fa4e857737a533a
Grinch101 <Masoud.Kameli@gmail.com>
2021-01-31 11:51:38 +0330

    using decorators to connect to posrgres

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, make_response
-from models.contact_sql import Contact
-from models.user_sql import User
-import names
-import random
-from functools import wraps
+from models.contact import Contact
+from models.user import User
+from utility.decor import sql_connection , login_required , path_set
+from utility.helpers import connection_closer , connection_opener , query
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
-
-####### Define Decorator #########
-def login_required(func):
-    @wraps(func)
-    def wrap(*args, **kwargs):
-        if request.cookies.get('user_id'):
-            return func(*args, **kwargs)
-        else:
-
-            return redirect(url_for(('login_form')))
-    return wrap
 
 #  injecting some functions to Jinja
 @app.context_processor
@@ -34,12 +23,12 @@ users_handler = User()
 
 ## View Function ##############
 
-
 @app.route('/', methods=["GET"])
+@sql_connection
 @login_required
-def index():
+def index(*args, **kwargs):
     userid = int(request.cookies.get('user_id'))
-    entry = users_handler.find_val(userid)
+    entry = users_handler.find_val(userid, **kwargs)
     username = entry['client_name']
     return render_template('index.html', username=username)
 
@@ -50,15 +39,18 @@ def login_form():
     return render_template('login.html')
 
 
-@app.route('/login_check', methods=["POST"])
-def login_check():
 
+@app.route('/login_check', methods=["POST"])
+@sql_connection
+def login_check(*args, **kwargs):
+
+    # cur = kwargs['cur']
     email = request.form.get("inputEmail")
     password = request.form.get("inputPassword")
 
-    if users_handler.validate(email, password):
+    if users_handler.validate(email, password, **kwargs):
 
-        userid = users_handler.find_userid_by_email(email)
+        userid = users_handler.find_userid_by_email(email , **kwargs)
         response = make_response(redirect(url_for('index')))
         response.set_cookie('user_id', str(userid))
         return response
@@ -70,13 +62,14 @@ def login_check():
 
 
 @app.route('/signup', methods=["GET"])
-def signup_form():
+def signup_form(*args, **kwargs):
 
     return render_template('signup.html')
 
 
 @app.route('/signup', methods=["POST"])
-def signup():
+@sql_connection
+def signup(*args, **kwargs):
 
     email = request.form.get("inputEmail")
     password = request.form.get("inputPassword")
@@ -86,10 +79,10 @@ def signup():
                 'password':password}
 
 
-    if not users_handler.old_user(email):
+    if not users_handler.old_user(email , **kwargs):
 
-        users_handler.add(entry)
-        userid = users_handler.find_userid_by_email(email)
+        users_handler.add(entry, **kwargs)
+        userid = users_handler.find_userid_by_email(email, **kwargs)
 
 
         response = make_response(redirect(url_for('index')))
@@ -100,47 +93,49 @@ def signup():
         return redirect(url_for('signup_form'))
 
 
+
 @app.route('/saved', methods=["POST"])
+@sql_connection
 @login_required
-def saved():
+def saved(*args, **kwargs):
 
     input_name = request.form['Name']
     input_number = request.form['Number']
     userid = request.cookies.get('user_id')
     userid = int(userid)
-    entry = users_handler.find_val(userid)
+    entry = users_handler.find_val(userid, **kwargs)
     client_name = entry['client_name']
 
-    value = {   'userid'        :userid,
-                # 'client_name'   :client_name,
-                'name'          :input_name ,
-                'phone'         :input_number}
+    # value = {   'userid'        :userid,
+    #             # 'client_name'   :client_name,
+    #             'name'          :input_name ,
+    #             'phone'         :input_number}
 
 
-    phonebook.add(value)
+    phonebook.add(userid , input_name, input_number , **kwargs)
 
     flash(f'{input_number} for {input_name} has been saved')
 
     return redirect(url_for('index'))
 
-
 @app.route('/table', methods=["GET"])
+@sql_connection
 @login_required
-def table():
+def table(*args, **kwargs):
 
     userid = request.cookies.get('user_id')
     userid = int(userid)
-    contact_list = phonebook.find_book(userid)
+    contact_list = phonebook.find_book(userid, **kwargs)
 
     return render_template('list.html', mylist=contact_list)
 
-
 @app.route('/delete', methods=["POST"])
+@sql_connection
 @login_required
-def delete():
+def delete(*args, **kwargs):
 
     id = int(request.form.get("DELETE"))
-    phonebook.delete(id)
+    phonebook.delete(id , **kwargs)
     flash(f"ID:{id} Deleted")
 
     return redirect(url_for('table'))
@@ -148,7 +143,7 @@ def delete():
 
 @app.route('/logout', methods=['POST'])
 @login_required
-def logout():
+def logout(*args, **kwargs):
 
     response = make_response(redirect(url_for('login_form')))
     response.set_cookie('user_id', "", max_age=0)
@@ -156,13 +151,14 @@ def logout():
 
 
 @app.route('/behind-the-scene', methods=['GET'])
-def behind():
+@sql_connection
+def behind(*args, **kwargs):
 
     if request.cookies.get('user_id'):
-        list1 = phonebook.get_all()
+        list1 = phonebook.get_all( **kwargs)
         userid = request.cookies.get('user_id')
         userid = int(userid)
-        list2 = phonebook.find_book(userid)
+        list2 = phonebook.find_book(userid, **kwargs)
         list3 = users_handler.list
         dic1 = phonebook.id_index
         # dic2 = users_handler.email_userid
@@ -176,7 +172,8 @@ def behind():
     else:
         return render_template('behind-the-scene.html')
 
-
+# import names
+# import random
 # i = 0
 # while i <= 150:
 
