6333b918542671cab95124be145bcb622a32493a
Grinch101 <Masoud.Kameli@gmail.com>
2021-02-11 23:23:23 +0330

    get_cursor defined (for pooling)

--- utility/helpers.py
+++ utility/helpers.py
@@ -1,11 +1,12 @@
 import psycopg2
-from psycopg2.pool import SimpleConnectionPool
-import psycopg2.extras
+from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extras import DictCursor
 from pathlib import Path
 from flask import g
 
 
-# DEFINE 3 HELPER FUNCTIONS:
+
+########### Helpers to work with one connection #####################
 
 def open_conn(host='localhost', database='phonebook', user='postgres', password=1):
     conn = psycopg2.connect(host=host,
@@ -13,27 +14,8 @@ def open_conn(host='localhost', database='phonebook', user='postgres', password=
                             user=user,
                             password=1)
 
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur = conn.cursor(cursor_factory=DictCursor)
     return cur, conn
-
-
-def open_pool(minconn, maxconn, /, host='localhost', database='phonebook', user='postgres', password=1):
-
-    connections = SimpleConnectionPool(minconn,
-                                       maxconn,
-                                       host=host,
-                                       database=database,
-                                       user=user,
-                                       password=1)
-
-    def conn():
-        return connections.getconn()
-
-    def cur():
-        conn = conn()
-        return conn.cursor()
-    
-    return cur(), conn()
 
 
 def close_conn(cur, conn):
@@ -42,6 +24,47 @@ def close_conn(cur, conn):
     conn.close()
 
 
+##################### Connection Pooling ###############
+
+class get_cursor():
+    def __init__(self, minconn, maxconn):
+        self.bag = []
+        self.host = 'localhost'
+        self.database = 'phonebook'
+        self.user = 'postgres'
+        self.password = 1
+        self.minconn = minconn
+        self.maxconn = maxconn
+        self.connections = ThreadedConnectionPool(self.minconn,
+                                                  self.maxconn,
+                                                  host=self.host,
+                                                  database=self.database,
+                                                  user=self.user,
+                                                  password=self.password)
+
+    def __enter__(self):
+        conns = self.connections
+        conn = conns.getconn()
+        self.bag.append(conn)
+        cur = conn.cursor(cursor_factory = DictCursor)
+        return cur
+
+
+    def run_and_fetch(self,query_text, vals,):
+        cursor = self.__enter__()
+        cursor.execute(query_text , vals)
+        try:
+            return cursor.fetchall()
+        except:
+            return f'no value to fetch'
+
+    def __exit__(self, *args):
+        conn = self.bag.pop()
+        conn.commit()
+        self.connections.putconn(conn)
+
+
+############ query func - working only with connection pooling ##########
 def query(path, query_name,  vals=''):
 
     file_name = query_name+'.sql'
@@ -51,12 +74,8 @@ def query(path, query_name,  vals=''):
         query_text = str(f.read())
         f.close()
     cur = g.cur
-    conn = g.conn
-
-    cur.execute(query_text,  vals)
-    try:
-        rows = cur.fetchall()
-        return rows
-    except:
-        pass
+    with cur:
+        rows = cur.run_and_fetch(query_text,  vals)
+        if rows != 'no value to fetch':
+            return rows
 
