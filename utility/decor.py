13dde773be9389b08dcac38883f1d81ffecd3dac
Grinch101 <Masoud.Kameli@gmail.com>
2021-02-11 23:23:02 +0330

    sql_pooling added

--- utility/decor.py
+++ utility/decor.py
@@ -1,8 +1,11 @@
 from flask import request, redirect, url_for, g
-from utility.helpers import close_conn , open_conn, query
+from utility.helpers import query,  get_cursor, close_conn
 from functools import wraps
 
+
 ####### Define Decorator #########
+
+
 def login_required(func):
     @wraps(func)
     def wrap(*args, **kwargs):
@@ -14,29 +17,44 @@ def login_required(func):
     return wrap
 
 
-################################
+############ work with one connection ##############
 def sql_connection(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        cur , conn = open_conn()
-        
+        cur, conn = open_conn()
+
         g.cur = cur
         g.conn = conn
-        output = func(*args, **kwargs )
-        close_conn(cur , conn)
+        output = func(*args, **kwargs)
+        close_conn(cur, conn)
         if output:
             return output
 
-    
+    return wrapper
+
+############ work with Connection pool ##############
+
+
+def sql_pooling(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        # cur, conn, pool = open_pool(1 ,10)
+        g.cur = get_cursor(1,10)
+        output = func(*args, **kwargs)
+        # put_conn(pool, conn)
+        if output:
+            return output
+
     return wrapper
 
 
-## another love making with decorator concept! :) 
-def path_set( path ):
+# another love making with decorator concept! :)
+def path_set(path):
     def query_decorator(func):
         def wrapper(*args, **kwargs):
 
             kwargs['path'] = path
-            return func(  *args, **kwargs)
+            return func(*args, **kwargs)
         return wrapper
     return query_decorator
