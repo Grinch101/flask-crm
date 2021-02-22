22e8feb706bacc3f220ff728e9c177a2799f80cc
Grinch101 <Masoud.Kameli@gmail.com>
2021-02-22 22:38:12 +0330

    use g.user_id instead of cookie

--- utility/decor.py
+++ utility/decor.py
@@ -1,17 +1,17 @@
 from flask import request, redirect, url_for, g
 from utility.helpers import query
 from functools import wraps
-
-
+from models.user import User
 ####### Define Decorator #########
+users_handler = User()
 
 
 def login_required(func):
     @wraps(func)
     def wrap(*args, **kwargs):
-        if request.cookies.get('user_id'):
+        if g.user_id is not None:
+            
             return func(*args, **kwargs)
         else:
-
             return redirect(url_for(('login_form')))
     return wrap
