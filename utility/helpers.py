a0685b548bc82ceddad072b838f6755add2e527f
Grinch101 <Masoud.Kameli@gmail.com>
2021-02-25 13:54:43 +0330

    plotter and datetime converter added

--- utility/helpers.py
+++ utility/helpers.py
@@ -3,11 +3,14 @@ from psycopg2.pool import ThreadedConnectionPool
 from psycopg2.extras import DictCursor
 from pathlib import Path
 from flask import g
-
+import datetime as dt
+import plotly
+import plotly.graph_objects as go
+import json
 
 ##### Create Connection pool ######
 def conn_pool(minconn, maxconn, /, host='localhost', database='phonebook', user='postgres', password=1):
-    
+
     return ThreadedConnectionPool(minconn,
                                   maxconn,
                                   host=host,
@@ -17,7 +20,7 @@ def conn_pool(minconn, maxconn, /, host='localhost', database='phonebook', user=
 
 
 ############ query func  ##########
-def query(query, vals=()):
+def query(query, vals=""):
 
     query = "sql/" + query + ".sql"
     path = Path(query)
@@ -25,6 +28,54 @@ def query(query, vals=()):
     with open(path, 'r') as f:
         query_text = str(f.read())
 
-    cur = g.conn.cursor(cursor_factory = DictCursor)
+    cur = g.conn.cursor(cursor_factory=DictCursor)
     cur.execute(query_text, vals)
     return cur
+
+
+############## convet date and time to datetime ############
+
+def conv_datetime(date, time):
+    date = str(date)
+    time = str(time)
+    # # reference for date formatting
+    # %d	Day of the month (decimal number)
+    # %m	Month (decimal number)
+    # %b	Month (abbreviated)
+    # %B	Month (full name)
+    # %y	Year (2 digit)
+    # %Y	Year (4 digit)
+    return dt.datetime.strptime(f'{date} {time}', '%Y-%m-%d %H:%M')
+
+
+def plotter(rows):
+    try:
+        actions = [row['action'] for row in rows]
+        dates = [row['datetime'] for row in rows]
+        notes = [row['description'] for row in rows]
+
+        lenght = range(1, len(actions)+1)
+
+        fig = go.Figure()
+        # date_time = [dt.datetime.strptime(item, "%Y-%m-%d %h:%m") for item in dates]
+
+        for i, j, k, l in zip(dates, actions, notes, lenght):
+
+            fig.add_trace(go.Scatter(mode='text', x=[i], y=[l+0.25], text=[k],  showlegend=True))
+            fig.add_trace(go.Scatter(mode='lines', x=[i], y=[0], line_color='black'))
+            fig.add_trace(go.Scatter(mode='markers', x=[i], y=[0], marker=dict(color='black',
+                                    line=dict(color='black')), name=j))
+                                    
+            fig.add_shape(x1=i, x0=i, y1=l, y0=0, line=dict(color='black'))
+
+        fig.update_layout(template="plotly_white", showlegend=False,
+                        yaxis=dict(showgrid=False, range=[0, 4], visible=False),
+                        xaxis=dict(showgrid=False, visible=True, showline=True, zeroline=True, zerolinewidth=2))
+
+        # JSONify:
+        return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        
+    except:
+        return {}
+
+
