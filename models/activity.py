b0be8a03bbfbf5de600fb047d5291d0a98dd77c2
Grinch101 <Masoud.Kameli@gmail.com>
2021-02-26 14:17:13 +0330

    define Activity() class for handling activity requests

--- models/activity.py
+++ models/activity.py
@@ -7,10 +7,9 @@ class Activity():
         pass
 
     def new_action(self, action, description, date_time, user_id, contact_id):
-        print('new action started')
         query('activity/insert', vals=(action,
                                        description, date_time, user_id, contact_id))
-        print('new action ended')
+
                                        
     def get_history(self, user_id, contact_id):
         return query('activity/history', vals=(user_id, contact_id))
