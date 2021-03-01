7ffb2c189c45bc59e12e05fdfe3065ad45c7092d
Grinch101 <Masoud.Kameli@gmail.com>
2021-03-01 12:16:13 +0330

    url_prefixes added

--- src/factory.py
+++ src/factory.py
@@ -7,9 +7,8 @@ from src.apps.contacts.views import contact
 def creat_app(config):
     app = Flask(__name__, template_folder='templates')
     app.config.from_object(config)
-
-    app.register_blueprint(user)
-    app.register_blueprint(activity)
-    app.register_blueprint(contact)
+    app.register_blueprint(user, url_prefix='/auth/')
+    app.register_blueprint(contact, url_prefix='/contact-management')
+    app.register_blueprint(activity, url_prefix='/log-activity/')
 
     return app
