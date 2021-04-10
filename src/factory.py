30346a581c597e3466f1ed8d8805dac039815a85
Grinch101 <Masoud.Kameli@gmail.com>
2021-04-11 02:31:29 +0430

    edit prefix for contact blueprint

--- src/factory.py
+++ src/factory.py
@@ -8,7 +8,7 @@ def creat_app(config):
     app = Flask(__name__, template_folder='templates')
     app.config.from_object(config)
     app.register_blueprint(user, url_prefix='/auth/')
-    app.register_blueprint(contact, url_prefix='/contact/')
+    app.register_blueprint(contact, url_prefix='/contacts/')
     app.register_blueprint(activity, url_prefix='/activity/')
 
     return app
