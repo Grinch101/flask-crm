0ac1802e62834723df8382063914a7e8e62b8d75
Grinch101 <Masoud.Kameli@gmail.com>
2021-04-16 01:34:04 +0430

    switch from cookie to JWT in the app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from src.config import DevelopmentConfig, ProductionConfig
 from utility.decor import login_required
 from utility.helpers import conn_pool
 from psycopg2 import DatabaseError, DataError
+from jwt import DecodeError
+import jwt
 
 
 # create app and register blueprints
@@ -26,19 +28,31 @@ def inject_func():
                 len=len,
                 int=int)
 
-
 ###### before and after each request ########
+@app.before_first_request
+@app.before_request
+def get_secret_key():
+    global app
+    secret_key = app.config['SECRET_KEY']
+    g.secret_key = secret_key
+
 @app.before_request
 def open_conn():
     global connections
     g.conn = connections.getconn()
 
-
 @app.before_request
-def user_ident():
-    if request.cookies.get('user_id'):
-        user_id = int(request.cookies.get('user_id'))
-        g.user = users_handler.get_by_id(user_id)
+def user_auth():
+    if request.headers.get('JWT'):
+        token = request.headers['JWT']
+        secret_key = g.secret_key
+        try:
+            data = jwt.decode(token, key = secret_key ,algorithms=["HS256"])
+            user_id = int(data['user_id'])
+            g.user =  users_handler.get_by_id(user_id)
+
+        except DecodeError as e:
+            return jsonify(f'Token invalid,  {e}')
     else:
         g.user = None
 
