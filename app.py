8e741a259c2fcb508a6d73c21667d44d1f3740c5
Grinch101 <Masoud.Kameli@gmail.com>
2021-04-30 18:49:27 +0430

    missing imports

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from utility.helpers import conn_pool, json_output
 from psycopg2 import DatabaseError, DataError
 from jwt import DecodeError
 import jwt
+import json
 
 
 # create app and register blueprints
@@ -43,22 +44,6 @@ def open_conn():
     g.conn = connections.getconn()
 
 
-# @app.before_request
-# def user_auth():
-#     if request.headers.get('JWT'):
-#         token = request.headers['JWT']
-#         secret_key = g.secret_key
-#         try:
-#             data = jwt.decode(token, key=secret_key, algorithms=["HS256"])
-#             user_id = int(data['user_id'])
-#             g.user = users_handler.get_by_id(user_id)
-
-#         except DecodeError as e:
-#             return json_output(error='Token invalid,  {e}', http_code= 403) # forbidden
-#     else:
-#         g.user = None
-
-
 @app.after_request
 def close_conn(response):
     if g.conn is not None:
@@ -80,7 +65,6 @@ def close_conn(response):
 
 
 ########### Error handler ###########
-
 @app.errorhandler(DataError)
 @app.errorhandler(DatabaseError)
 def rollback_changes(error):
@@ -89,7 +73,6 @@ def rollback_changes(error):
     global connections
     connections.putconn(g.conn)
     g.conn = None
-
     return json_output(error = f"Database Error! {error}", http_code =500)
 
 
