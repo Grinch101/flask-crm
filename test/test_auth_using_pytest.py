f89c1aff986b284edf7ebb7c1f4f93a3f9f7aa11
Grinch101 <Masoud.Kameli@gmail.com>
2021-05-01 11:23:19 +0430

    test auth using pytest

--- test/test_auth_using_pytest.py
+++ test/test_auth_using_pytest.py
@@ -3,10 +3,9 @@
 from app import app
 from src.config import TestingConfig
 import pytest
-from flask import g, Response, Request, request, make_response
 import json
-import flask
-import requests
+from flask import g, Response
+
 
 @pytest.fixture
 def app_runner():
@@ -28,7 +27,7 @@ def client(app_runner):
         yield client
 
 
-## TESTS:
+# TESTS:
 
 def test_g_contents(app_ctx):
     with app.test_request_context():
@@ -44,7 +43,7 @@ def test_signup(client):
                      data={'inputEmail': 'SomeEmail@gmail.com',
                            'inputPassword': 'A',
                            'client_name': 'NedaForTest'}
-                           )
+                     )
     data = json.loads(rs.data)['data']
     error = json.loads(rs.data)['error']
     assert error is None
@@ -52,12 +51,12 @@ def test_signup(client):
     assert rs.status_code == 201
 
 
-def test_signup2(app_runner): # using request context instead of test_client
+def test_signup2(app_runner):  # using request context instead of test_client
     with app.test_request_context('/auth/signup',
-                                data={'inputEmail': 'Neda',
-                                      'inputPassword': 'Neda',
-                                      'client_name': 'NedaForTest'},
-                                method="POST"):
+                                  data={'inputEmail': 'Neda',
+                                        'inputPassword': 'Neda',
+                                        'client_name': 'NedaForTest'},
+                                  method="POST"):
         app.preprocess_request()
         rs = app.dispatch_request()
         rs = app.process_response(rs)
@@ -70,12 +69,12 @@ def test_signup2(app_runner): # using request context instead of test_client
 
 def test_login(client):
     rs = client.post('/auth/login',
-                    data={'inputEmail': 'Masoud@gmail.com', 
-                    # this user is present in database permanently
-                        'inputPassword': 'A'} )
+                     data={'inputEmail': 'Masoud@gmail.com',
+                           # this user is present in database permanently
+                           'inputPassword': 'A'})
     data = json.loads(rs.data)['data']
     error = json.loads(rs.data)['error']
-    with open('token.txt','w') as f:
+    with open('token.txt', 'w') as f:
         f.write(data)
         f.close()
     assert rs.status_code == 200
@@ -87,28 +86,28 @@ def test_login(client):
 def test_login2(app_runner):
     with app.test_client() as client:
         rs = client.post('/auth/signup',
-                        data={'inputEmail': 'SomeNewEmail@gmail.com',
-                            'inputPassword': 'A',
-                            'client_name': 'ForTest'}
-                            )
+                         data={'inputEmail': 'SomeNewEmail@gmail.com',
+                               'inputPassword': 'A',
+                               'client_name': 'ForTest'}
+                         )
         data = json.loads(rs.data)['data']
         error = json.loads(rs.data)['error']
         assert error is None
         assert data is not None
         assert rs.status_code == 201
         rs2 = client.post('/auth/login',
-                    data={'inputEmail': 'SomeNewEmail@gmail.com',
-                        'inputPassword': 'A'})
+                          data={'inputEmail': 'SomeNewEmail@gmail.com',
+                                'inputPassword': 'A'})
         data = json.loads(rs2.data)['data']
         error = json.loads(rs2.data)['error']
-        assert error is None 
-        assert rs2.status_code == 200 
+        assert error is None
+        assert rs2.status_code == 200
 
 
 def test_current_user(client):
-    with open('token.txt','r') as f:
+    with open('token.txt', 'r') as f:
         token = f.read()
-        print(token) 
+        print(token)
         # read the token written in the previous test
         # for the permanent user (test: mark.three)
     rs = client.get('/auth/current_user',
@@ -122,28 +121,26 @@ def test_current_user(client):
 
 def test_login3(app_runner):
     with app.test_request_context('/auth/signup',
-                            data={'inputEmail': 'Neda2',
-                                    'inputPassword': 'Neda',
-                                    'client_name': 'NedaForTest'},
-                            method="POST"):
+                                  data={'inputEmail': 'Neda2',
+                                        'inputPassword': 'Neda',
+                                        'client_name': 'NedaForTest'},
+                                  method="POST"):
         app.preprocess_request()
         rs = app.dispatch_request()
         with app.test_request_context('/auth/login',
-                            data={'inputEmail': 'Neda2',
-                                    'inputPassword': 'Neda'},
-                            method="POST"):
+                                      data={'inputEmail': 'Neda2',
+                                            'inputPassword': 'Neda'},
+                                      method="POST"):
             app.preprocess_request()
             rs1 = app.dispatch_request()
             # app.process_response(rs)
             # app.process_response(rs1)
     token = json.loads(rs1.data)['data']
     error = json.loads(rs1.data)['error']
-    with open('new_user_token.txt','w') as f:
+    with open('new_user_token.txt', 'w') as f:
         f.write(error)
         f.close()
     assert token is not None
     assert error is None
     assert rs.status_code == 201
     assert rs1.status_code == 200
-
-    
