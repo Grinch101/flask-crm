b9b5aca5312c0d8f9f3166a3d6f18405652a0a7f
Grinch101 <Masoud.Kameli@gmail.com>
2021-05-04 15:20:23 +0430

    modified scopes/ define new user as fixture for further use

--- test/conftest.py
+++ test/conftest.py
@@ -6,23 +6,38 @@ import pytest
 import json
 
 
-@pytest.fixture(autouse=True, scope='module')
+@pytest.fixture(scope='session')
 def _app_runner():
     app = creat_app(TestingConfig)
-    yield app
+    return app
 
 
-@pytest.fixture(autouse=True, scope='module')
+@pytest.fixture(scope='session')
 def client(_app_runner):
-    conns = conn_pool(1,10)
+    conns = conn_pool(1, 10)
     conn = conns.getconn()
     with _app_runner.app_context():
         g.conn = conn
         g.secret_key = _app_runner.config['SECRET_KEY']
         with _app_runner.test_client() as client:
             yield client
+            conn = g.conn
+            g.conn = None
             conn.rollback()
             conns.putconn(conn)
+
+
+@pytest.fixture(scope='session')
+def TOKEN(client):
+    rs1 = client.post('/auth/signup',
+                      data={'inputEmail': 'SomeEmail@gmail.com',
+                            'inputPassword': 'A',
+                            'client_name': 'ForTest'})
+    rs2 = client.post('/auth/login',
+                      data={'inputEmail': 'SomeEmail@gmail.com',
+                            'inputPassword': 'A'})
+    info, TOKEN, error = extract(rs2)
+    return TOKEN
 
 
 def extract(response):
