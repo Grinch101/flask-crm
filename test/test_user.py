97818cb8b2cdf64893804a840cff05b46931dd17
Grinch101 <Masoud.Kameli@gmail.com>
2021-05-04 15:21:00 +0430

    separate tests for user

--- test/test_user.py
+++ test/test_user.py
@@ -1,11 +1,12 @@
 import json
+import pytest
 from test.conftest import extract
 
-def test_user_bp(client):
 
+def test_signup(client):
     # TEST USER SIGNUP
     rs1 = client.post('/auth/signup',
-                      data={'inputEmail': 'SomeEmail@gmail.com',
+                      data={'inputEmail': "EMAIL",
                             'inputPassword': 'A',
                             'client_name': 'ForTest'})
     info, data, error = extract(rs1)
@@ -13,15 +14,20 @@ def test_user_bp(client):
     assert data is not None
     assert rs1.status_code == 201
 
+
+def test_login(client):
     # TESTING USER LOGIN AND TOKEN GENERATION
     rs2 = client.post('/auth/login',
-                      data={'inputEmail': 'SomeEmail@gmail.com',
+                      data={'inputEmail': 'EMAIL',
                             'inputPassword': 'A'})
     info, TOKEN, error = extract(rs2)
     assert error is None
     assert TOKEN is not None
     assert rs2.status_code == 200
 
+
+def test_current_user(client, TOKEN):
+    # TOKEN is a session fixture assigned to user "ForTest"
     # TESTING USER RETRIVE
     rs3 = client.get('/auth/current_user',
                      headers={'JWT': TOKEN})
@@ -31,7 +37,9 @@ def test_user_bp(client):
     assert data['email'] == 'SomeEmail@gmail.com'
     assert rs3.status_code == 200
 
-    # TESTING UPDATE
+
+def test_user_update(client, TOKEN):
+    # TEST USER UPDATE
     rs4 = client.put('/auth/user_update',
                      headers={'JWT': TOKEN},
                      data={'new_name': 'name_changed'})
